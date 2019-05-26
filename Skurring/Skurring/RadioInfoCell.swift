b6b77b96fe8f70ba6c726d8bcb0491beb9fe4f3b
ChartaApps <marius.fagerhol@yahoo.com>
2019-05-26 20:17:38 +0200

    Fixed tableview and cell

--- Skurring/Skurring/RadioInfoCell.swift
+++ Skurring/Skurring/RadioInfoCell.swift
@@ -17,43 +17,7 @@ class RadioInfoCell: UITableViewCell {
 
     func updateUI(information: RadioPlayer){
         self.radioInfo.text = information.radioINFO
-        
-        self.theImage.alpha = 0
-        dataTask?.cancel()
-        if let theSavedImage = DS.dsInstance.stationImageCache.object(forKey: information.imgPNG as NSString) {
-            self.theImage.image = theSavedImage
-            self.theImage.alpha = 1
-        } else {
-            loadImage(url: information.imgPNG) { (theImage) in
-                if let loadedImage = theImage {
-                    self.theImage.image = loadedImage
-                    self.theImage.alpha = 1
-                }
-            }
-        }
-    }
-    
-    private func loadImage(url: String, done: @escaping (_ loadedImage: UIImage?) -> ()) {
-        if let theUrl = URL(string: url) {
-           dataTask = URLSession.shared.dataTask(with: theUrl) { (data, response, error) in
-                DispatchQueue.main.async {
-                    guard let data = data else {
-                        print("There is no data")
-                        done(nil)
-                        return
-                    }
-                    if let theImage = UIImage(data: data) {
-                        print("Got the image, the image should be set")
-                        DS.dsInstance.stationImageCache.setObject(theImage, forKey: url as NSString)
-                        done(theImage)
-                    } else {
-                        print("Something is wrong with the data")
-                        done(nil)
-                    }
-                }
-            }
-            dataTask?.resume()
-        }
+        self.theImage.image = UIImage(named: information.imgPNG)
     }
 
 

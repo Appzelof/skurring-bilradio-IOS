d12a59967137d3ea7ec083feeceeb4dc744d4b39
Daniel Bornstedt <appzelof@gmail.com>
2020-09-06 22:23:03 +0200

    Refactored fetchRadioStation

--- skurring/Network/NetworkManager.swift
+++ skurring/Network/NetworkManager.swift
@@ -17,35 +17,6 @@ final class NetworkManager {
     private let firebaseData = FirebaseDataFetcher()
     private let imageProvider = ImageProvider()
 
-    func fetchRadioData(at completion: @escaping ([RadioStation]) -> Void) {
-        if radioStations.isEmpty {
-            firebaseData.fetchData { (station) in
-                guard
-                    let station = station,
-                    let name = station.name,
-                    let radioCountry = station.radioCountry,
-                    let imageUrl = station.imageURL,
-                    let radioURL = station.radioURL,
-                    let radioHQURL = station.radioHQURL
-
-                    else { return }
-
-                self.radioStations.append(
-                    RadioStation(
-                        imageURL: imageUrl,
-                        name: name,
-                        radioCountry: radioCountry,
-                        radioURL: radioURL,
-                        radioHQURL: radioHQURL
-                    )
-                )
-                completion(self.radioStations)
-            }
-        } else {
-            completion(self.radioStations)
-        }
-    }
-
     func fetch<T: Decodable>(url: URL, model: T.Type) -> AnyPublisher<T, Error> {
         return URLSession.shared.dataTaskPublisher(for: url)
                 .map { $0.data }
@@ -57,6 +28,10 @@ final class NetworkManager {
 
     func fetchRadioImage(url: String, completion: @escaping (UIImage) -> ()) {
         imageProvider.fetchImage(url: url, completion: completion)
+    }
+
+    func fetchRadioStation(at completion: @escaping (RadioStation?) -> Void) {
+        firebaseData.fetchData(completion: completion)
     }
 }
 

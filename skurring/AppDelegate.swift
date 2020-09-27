ad51aa2ad588f001f617ddcc848c8d12c191691d
Daniel Bornstedt <appzelof@gmail.com>
2020-09-27 23:40:36 +0200

    Onboarding screen is only visible when app is launched for the first time

--- skurring/AppDelegate.swift
+++ skurring/AppDelegate.swift
@@ -27,9 +27,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window?.rootViewController = tabBarVC
         window?.makeKeyAndVisible()
 
-//     if !UserDefaults().bool(forKey: ConstantHelper.firstLaunch) {
-        tabBarVC.present(OnboardingViewController(), animated: true, completion: nil)
-//      }
+        if !UserDefaults().bool(forKey: ConstantHelper.firstLaunch) {
+            tabBarVC.present(OnboardingViewController(), animated: true, completion: nil)
+        }
 
         AppRatingManager.shared.handleSKStoreReview()
 
